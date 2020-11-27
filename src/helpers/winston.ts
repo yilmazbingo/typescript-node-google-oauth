8f382496e04d0ce4f1dd44587e6d82921fba3230
tesla <tesla@tesla.home>
2020-11-27 18:04:35 -0500

    winston

--- src/helpers/winston.ts
+++ src/helpers/winston.ts
@@ -1,14 +1,14 @@
-// import MongoDB, { MongoDBTransportInstance } from "winston-mongodb";
+import MongoDB, { MongoDBTransportInstance } from "winston-mongodb";
 
-// import winston from "winston";
+import winston from "winston";
 
-// export const logger = winston.createLogger({
-//   level: "info",
-//   format: winston.format.json(),
-//   defaultMeta: { service: "user-service" },
-//   transports: [
-//     new winston.transports.File({ filename: "error.log", level: "error" }),
-//     new MongoDB.MongoDB({ db: process.env.DATABASE_URL! }),
-//     // new winston.transports.
-//   ],
-// });
+export const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  defaultMeta: { service: "user-service" },
+  transports: [
+    new winston.transports.File({ filename: "error.log", level: "error" }),
+    new MongoDB.MongoDB({ db: process.env.DATABASE_URL! }),
+    // new winston.transports.
+  ],
+});
