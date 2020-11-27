1d72f92e78e06490aa47ca6534bd5062135ee12f
tesla <tesla@tesla.home>
2020-11-27 04:15:13 -0500

    winston logger issue

--- src/helpers/winston.ts
+++ src/helpers/winston.ts
@@ -1,21 +1,14 @@
-import MongoDB, { MongoDBTransportInstance } from "winston-mongodb";
+// import MongoDB, { MongoDBTransportInstance } from "winston-mongodb";
 
-// const a=MongoDB:MongoDBTransportInstance
-// const MongoDB: MongoDBTransportInstance;
+// import winston from "winston";
 
-// const transport = new MongoDB({
-// 	//...
+// export const logger = winston.createLogger({
+//   level: "info",
+//   format: winston.format.json(),
+//   defaultMeta: { service: "user-service" },
+//   transports: [
+//     new winston.transports.File({ filename: "error.log", level: "error" }),
+//     new MongoDB.MongoDB({ db: process.env.DATABASE_URL! }),
+//     // new winston.transports.
+//   ],
 // });
-
-import winston from "winston";
-
-export const logger = winston.createLogger({
-  level: "info",
-  format: winston.format.json(),
-  defaultMeta: { service: "user-service" },
-  transports: [
-    new winston.transports.File({ filename: "error.log", level: "error" }),
-    new MongoDB.MongoDB({ db: process.env.DATABASE_URL! }),
-    // new winston.transports.
-  ],
-});
