d07b92f6afa5f56079f0d53a44f589a047848064
tesla <tesla@tesla.home>
2020-12-07 10:44:34 -0500

    fixing oauth

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-// app.use(favicon(path.join(__dirname, "..", "public", "favicon.ico")));
+app.use(favicon(path.join(__dirname, "..", "public", "favicon.ico")));
 
 app.use(cors());
 // app.use("/images", express.static("images"));
